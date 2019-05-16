5b8c0f35f39b49bb013ff2430d6541ebc02fbf47
João Antônio <jacs@cin.ufpe.br>
2019-05-16 03:18:02 -0300

    Stock Screen mock implemented

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, ScrollView} from 'react-native';
+import Stock from "./modules/stock.js";
 
 export default class App extends Component {
   render() {
     return (
-      <ScrollView style={styles.container}>
+      <Stock></Stock>
+      /*<ScrollView style={styles.container}>
         <View style={styles.postContainer}>
           <Text style={styles.postTitle}>Lorem Ipsum</Text>
           <Text style={styles.postDescription}>dolor sit amet</Text>
@@ -24,7 +26,7 @@ export default class App extends Component {
           <Text style={styles.postTitle}>Lorem Ipsum</Text>
           <Text style={styles.postDescription}>dolor sit amet</Text>
         </View>
-      </ScrollView>
+      </ScrollView>*/
     );
   }
 }
@@ -32,7 +34,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333',
+    backgroundColor: '#531382',
   },
   postContainer: {
     padding: 20,
